eeaf2b75552bdac1e242cedc6634959fe9f022ca
tera-tools <none@none.com>
2020-02-03 00:25:01 -0500

    updated for v88

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-module.exports = function GmDetector(dispatch) {
+module.exports = function GmDetector(mod) {
 
-	dispatch.hook('S_SPAWN_USER', 13, event => {
-        if (event.gm) {
-            let message = '[GM] ' + event.name + ' Detected!';
-            if (event.gmInvisible) message += ' (Invisible)';           
-            
-            // send messages to client
-            dispatch.toClient('S_CHAT', 1, {
-                channel: 7,
-                authorName: '',
-                message: message
-            }); 
-            
-            dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
-                unk1: 42, // 42 Blue Shiny Text, 31 Normal Text
-                unk2: 0,
-                unk3: 27,
-                message: message
-            }); 
-            
-            // show hidden GMs?
-            event.gmInvisible = false;
-            return true;
-        }
-    });
-    
+	mod.hook('S_SPAWN_USER', 15, event => {
+    if (event.gm) {
+      let message = '[GM] ' + event.name + ' Detected!';
+      if (event.gmInvisible) message += ' (Invisible)';           
+      
+      // send messages to client
+      mod.command.message(message);
+
+      let msgObject = {
+        message: text,
+        type: 42, // 42 Blue Shiny Text, 31 Normal Text
+        chat: 0, 
+        channel: 27
+      };      
+      mod.send("S_DUNGEON_EVENT_MESSAGE", 2, msgObject);
+      
+      // show hidden GMs?
+      event.gmInvisible = false;
+      return true;
+    }
+  });
 }
